f1eca7eccd7bce1f7cd7807cf0a68c45064aa700
imhalid <imhalid@icloud.com>
2024-03-14 12:51:41 +0300

    solution quiz1

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,15 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
 
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(value: u16) -> u16 {
+    if (value > 40) {
+        return value;
+    } else {
+        return value * 2;
+    }
+}
 
 // Don't modify this function!
 #[test]
